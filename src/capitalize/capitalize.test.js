581ac394f69a032416bc2b2b21cb71cd03c69f1d
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-11 22:30:56 +0200

    Add 1 more test func for already capitalized strings

--- src/capitalize/capitalize.test.js
+++ src/capitalize/capitalize.test.js
@@ -1,5 +1,11 @@
 import { capitalize } from "./capitalize";
 
+// Return capitalized string
 test('Capitalize the first letter of a string', () => {
-  expect(capitalize('vova')).toEqual('Vova');
+  expect(capitalize('string')).toEqual('String');
+});
+
+// Return same string if already capitalized
+test('Return same string if already capitalized', () => {
+  expect(capitalize('String')).toEqual('String');
 });
