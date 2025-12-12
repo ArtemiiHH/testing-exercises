33174dc518c45d01486db0ecc2707a6cfcc835bb
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-12 12:33:42 +0200

    Write functions and test

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -1,17 +1,17 @@
-
+import { Calculator } from "./calculator";
 
 test('Add two numbers', () => {
-  expect(add(2 + 2)).toBe(4);
+  expect(Calculator.add(2, 2)).toBe(4);
 });
 
 test('Subtract two numbers', () => {
-  expect(subtract(4 - 2)).toBe(2);
+  expect(Calculator.subtract(4, 2)).toBe(2);
 });
 
 test('Multiply two numbers', () => {
-  expect(multiply(5 * 2)).toBe(10);
+  expect(Calculator.multiply(5, 2)).toBe(10);
 });
 
 test('Divide two numbers', () => {
-  expect(divide(10 / 2)).toBe(5);
+  expect(Calculator.divide(10, 2)).toBe(5);
 });
