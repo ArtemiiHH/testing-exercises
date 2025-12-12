b105ca07005cd202639f66f841e1cc991b662d9c
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-12 11:09:45 +0200

    Write failing tests for each func

---
+++ src/calculator/calculator.test.js
@@ -0,0 +1,17 @@
+
+
+test('Add two numbers', () => {
+  expect(add(2 + 2)).toBe(4);
+});
+
+test('Subtract two numbers', () => {
+  expect(subtract(4 - 2)).toBe(2);
+});
+
+test('Multiply two numbers', () => {
+  expect(multiply(5 * 2)).toBe(10);
+});
+
+test('Divide two numbers', () => {
+  expect(divide(10 / 2)).toBe(5);
+});
