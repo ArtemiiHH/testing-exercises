2079743713087bddaea7e06660cb1957739a6917
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-12 14:25:50 +0200

    Add case sensitive test + import func

--- src/caesarCipher/caesarCipher.test.js
+++ src/caesarCipher/caesarCipher.test.js
@@ -1,5 +1,10 @@
+import { caesarCipher } from "./caesarCipher";
 
 
 test('Alphabet letter wrapping', () => {
   expect(caesarCipher('xyz', 3)).toEqual('abc');
+});
+
+test('Case preservation', () => {
+  expect(caesarCipher('HeLLo', 3)).toEqual('KhOOr');
 });
