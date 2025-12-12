9c700158d553a5e529322feae7b77041b6143e6b
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-12 14:38:44 +0200

    Write test for character punctuation

--- src/caesarCipher/caesarCipher.test.js
+++ src/caesarCipher/caesarCipher.test.js
@@ -1,10 +1,16 @@
 import { caesarCipher } from "./caesarCipher";
 
-
+// Return wrapping
 test('Alphabet letter wrapping', () => {
   expect(caesarCipher('xyz', 3)).toEqual('abc');
 });
 
+// Return case preserved string
 test('Case preservation', () => {
   expect(caesarCipher('HeLLo', 3)).toEqual('KhOOr');
+});
+
+// Return punctuation
+test('Return string with punctuation', () => {
+  expect(caesarCipher('Hello, World!', 3)).toEqual('Khoor, Zruog!');
 });
