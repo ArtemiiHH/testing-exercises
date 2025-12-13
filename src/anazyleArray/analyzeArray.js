a17ccd23ecbc751e3c01d07672940d4edf5939bc
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-13 18:59:44 +0200

    Write and test func

--- src/anazyleArray/analyzeArray.js
+++ src/anazyleArray/analyzeArray.js
@@ -1,3 +1,10 @@
 export function analyzeArray(arr) {
+  // Variables to return
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const average = arr.reduce((a, b) => a + b) / arr.length;
+  const length = arr.length;
 
+  // Return values
+  return { average, min, max, length };
 }
